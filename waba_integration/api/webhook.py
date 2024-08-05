61a07581689ebbb0164e995ddf3ccf88168df1bd
idmlabhishek <abhishek@idesignmylife.net>
2024-08-05 05:40:31 +0000

    testing webhook for whatsapp

--- waba_integration/api/webhook.py
+++ waba_integration/api/webhook.py
@@ -9,11 +9,16 @@ from werkzeug.wrappers import Response
 
 @frappe.whitelist(allow_guest=True)
 def handle():
+	
 	if frappe.request.method == "GET":
 		return verify_token_and_fulfill_challenge()
 
 	try:
 		form_dict = frappe.local.form_dict
+		frappe.get_doc(
+			{"doctype": "WABA Webhook Log", "payload": frappe.as_json(form_dict)}
+		).insert(ignore_permissions=True)
+
 		messages = form_dict["entry"][0]["changes"][0]["value"].get("messages", [])
 		statuses = form_dict["entry"][0]["changes"][0]["value"].get("statuses", [])
 
@@ -23,9 +28,7 @@ def handle():
 		for message in messages:
 			create_waba_whatsapp_message(message)
 
-		frappe.get_doc(
-			{"doctype": "WABA Webhook Log", "payload": frappe.as_json(form_dict)}
-		).insert(ignore_permissions=True)
+		
 	except Exception:
 		frappe.log_error("WABA Webhook Log Error", frappe.get_traceback())
 		frappe.throw("Something went wrong")
